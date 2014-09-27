181c524df05397856d1ac15ebc315d175423528d
Mysterion <pericov.ko@gmail.com>
2014-09-27 23:53:45 +0400

    Initial JavaFx version of test

---
+++ src/main/java/org/mystic/gotshild/Controller.java
@@ -0,0 +1,144 @@
+package org.mystic.gotshild;
+
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.scene.Group;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
+public class Controller {
+    public static final int SIZE = 30;
+    @FXML
+    Group rootGroup;
+    @FXML
+    Button startTestButton;
+    @FXML
+    TextField userName;
+    @FXML
+    Label label;
+    @FXML
+    ImageView imageView;
+    @FXML
+    ImageView patternA;
+    @FXML
+    ImageView patternB;
+    @FXML
+    ImageView patternV;
+    @FXML
+    ImageView patternG;
+    @FXML
+    ImageView patternD;
+    private PrintWriter out;
+    private String login;
+    private int currentImage = 1;
+
+    @FXML
+    private void clearTextField() {
+        userName.clear();
+    }
+
+    @FXML
+    private void closeWindow() {
+        if (null != out) {
+            out.flush();
+            out.close();
+        }
+        Platform.exit();
+    }
+
+    @FXML
+    private void startGotshildTest() throws FileNotFoundException {
+        if (!userName.getText().equalsIgnoreCase("Введите свое имя") && userName.getText().length() > 0) {
+            out = new PrintWriter((userName.getText() + (System.currentTimeMillis() % 100000)) + ".txt");
+            this.login = userName.getText();
+            out.println(login);
+            startTestButton.setDisable(true);
+            userName.setVisible(false);
+            setPatternsVisible(true);
+            imageView.setVisible(false);
+            int step = 1;
+            goNextStep(step);
+        }
+    }
+
+    private void setPatternsVisible(boolean flag) {
+        patternA.setVisible(flag);
+        patternB.setVisible(flag);
+        patternV.setVisible(flag);
+        patternG.setVisible(flag);
+        patternD.setVisible(flag);
+    }
+
+    @FXML
+    private void nextInstruction() {
+        if (currentImage < 3) {
+            currentImage++;
+            final String url = Controller.class.getResource(getPathToRes() +
+                    "instruction-" + currentImage + ".jpg").toString();
+            imageView.setImage(new Image(url));
+        }
+    }
+
+    private String getPathToRes() {
+        int t = Controller.class.getPackage().getName().split("\\.").length;
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < t; ++i) {
+            sb.append("../");
+        }
+        return sb.toString();
+    }
+
+    private void goNextStep(int step) {
+        if (step > SIZE) {
+            setPatternsVisible(false);
+            return;
+        }
+        final long startTime = System.currentTimeMillis();
+        String url = Controller.class.getResource(getPathToRes() + "tasks/" + step + ".jpg").toString();
+        System.out.println(url);
+        ImageView task = new ImageView(new Image(url));
+        task.setLayoutX(100);
+        task.setLayoutY(250);
+        task.setFitWidth(170);
+        task.setFitHeight(136);
+        task.setPreserveRatio(true);
+        task.setPickOnBounds(true);
+        task.setVisible(true);
+        patternA.setOnMouseClicked(mouseEvent -> {
+            outputData(step, "А", startTime);
+            intoRecursion(step, task);
+        });
+        patternB.setOnMouseClicked(mouseEvent -> {
+            outputData(step, "Б", startTime);
+            intoRecursion(step, task);
+        });
+        patternV.setOnMouseClicked(mouseEvent -> {
+            outputData(step, "В", startTime);
+            intoRecursion(step, task);
+        });
+        patternG.setOnMouseClicked(mouseEvent -> {
+            outputData(step, "Г", startTime);
+            intoRecursion(step, task);
+        });
+        patternD.setOnMouseClicked(mouseEvent -> {
+            outputData(step, "Д", startTime);
+            intoRecursion(step, task);
+        });
+        rootGroup.getChildren().add(task);
+    }
+
+    private void intoRecursion(int step, ImageView task) {
+        rootGroup.getChildren().remove(task);
+        goNextStep(step + 1);
+    }
+
+    private void outputData(int step, String patternName, long startTime) {
+        out.println(step + " \t" + patternName + " \t" + (System.currentTimeMillis() - startTime) + " \t");
+    }
+}
---
+++ src/main/java/org/mystic/gotshild/Main.java
@@ -0,0 +1,24 @@
+package org.mystic.gotshild;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.initStyle(StageStyle.UNDECORATED);
+        Group root = FXMLLoader.load(getClass().getResource("/fxml/main.fxml"));
+        primaryStage.setTitle("Gotshild Test");
+        primaryStage.setScene(new Scene(root, 800, 600));
+        primaryStage.show();
+    }
+}
